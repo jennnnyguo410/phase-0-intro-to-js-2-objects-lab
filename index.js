9c2161cde3090f20bd832db15d825e7b7d5223e7
Hao Guo <jennnnyguo@gmail.com>
2023-03-26 16:37:06 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jenny",
+    streetAddress: "2 Fenton Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, vlaue) {
+    return {
+        ...employee,
+        [key]: vlaue
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, vlaue) {
+    employee[key] = vlaue;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, vlaue) {
+    const newObj = { ...employee };
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, vlaue) {
+    delete employee[key];
+    return employee;
+}
